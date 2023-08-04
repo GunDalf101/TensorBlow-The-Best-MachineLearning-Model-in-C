fa3e91cf1c21d62841bed78058559b9a3e786d35
Mouad Bennani <mouadgaming3@gmail.com>
2023-08-04 05:04:35 +0100

    Xor experiment succes

--- brain.c
+++ brain.c
@@ -1,20 +1,157 @@
 #include "brain.h"
 
+typedef struct {
+    Matrix a0, a1, a2;
+    Matrix w1, b1;
+    Matrix w2, b2;
+} neuralNet;
+
+neuralNet XorInit(size_t input_size, size_t hidden_size, size_t output_size){
+    neuralNet Xor;
+    Xor.a0 = matrix_init(1, input_size);
+    Xor.w1 = matrix_init(input_size, hidden_size);
+    Xor.b1 = matrix_init(1, hidden_size);
+    Xor.a1 = matrix_init(1, hidden_size);
+    Xor.w2 = matrix_init(hidden_size, output_size);
+    Xor.b2 = matrix_init(1, output_size);
+    Xor.a2 = matrix_init(1, output_size);
+    return Xor;
+}
+
+float XorForward(neuralNet Xor) {
+    matrix_dot(Xor.a1, Xor.a0, Xor.w1);
+    matrix_add(Xor.a1, Xor.a1, Xor.b1);
+    sigmoid_activation(Xor.a1);
+    matrix_dot(Xor.a2, Xor.a1, Xor.w2);
+    matrix_add(Xor.a2, Xor.a2, Xor.b2);
+    sigmoid_activation(Xor.a2);
+    return MATRIX_AT(Xor.a2, 0, 0);
+}
+
+float td[] = {
+    0, 0, 0,
+    0, 1, 1,
+    1, 0, 1,
+    1, 1, 0
+};
+
+float model_cost (neuralNet M, Matrix train_input, Matrix train_output){
+    assert(train_input.rows == train_output.rows);
+    assert(train_output.cols == M.a2.cols);
+    size_t n = train_input.rows;
+    float cost = 0;
+
+    for (size_t i = 0; i < n; i++){
+        Matrix x = matrix_init(1, train_input.cols);
+        Matrix y = matrix_init(1, train_output.cols);
+        matrix_row(x, i, train_input);
+        matrix_row(y, i, train_output);
+        matrix_copy(M.a0, x);
+        XorForward(M);
+        size_t m = train_input.cols;
+        for (size_t j = 0; j < m; j++){
+            float d = MATRIX_AT(M.a2, 0, j) - MATRIX_AT(y, 0, j);
+            cost += pow(d, 2);
+        }
+    }
+    return cost / n;
+}
+
+void finite_difference (neuralNet M, neuralNet g, float epsilon, Matrix train_input, Matrix train_output){
+    float saved;
+    float cost = model_cost(M, train_input, train_output);
+
+    for (size_t i = 0; i < M.w1.rows; i++){
+        for (size_t j = 0; j < M.w1.cols; j++){
+            saved = MATRIX_AT(M.w1, i, j);
+            MATRIX_AT(M.w1, i, j) += epsilon;
+            MATRIX_AT(g.w1, i, j) = (model_cost(M, train_input, train_output) - cost) / epsilon;
+            MATRIX_AT(M.w1, i, j) = saved;
+        }
+    }
+    for (size_t i = 0; i < M.b1.rows; i++){
+        for (size_t j = 0; j < M.b1.cols; j++){
+            saved = MATRIX_AT(M.b1, i, j);
+            MATRIX_AT(M.b1, i, j) += epsilon;
+            MATRIX_AT(g.b1, i, j) = (model_cost(M, train_input, train_output) - cost) / epsilon;
+            MATRIX_AT(M.b1, i, j) = saved;
+        }
+    }
+    for (size_t i = 0; i < M.w2.rows; i++){
+        for (size_t j = 0; j < M.w2.cols; j++){
+            saved = MATRIX_AT(M.w2, i, j);
+            MATRIX_AT(M.w2, i, j) += epsilon;
+            MATRIX_AT(g.w2, i, j) = (model_cost(M, train_input, train_output) - cost) / epsilon;
+            MATRIX_AT(M.w2, i, j) = saved;
+        }
+    }
+    for (size_t i = 0; i < M.b2.rows; i++){
+        for (size_t j = 0; j < M.b2.cols; j++){
+            saved = MATRIX_AT(M.b2, i, j);
+            MATRIX_AT(M.b2, i, j) += epsilon;
+            MATRIX_AT(g.b2, i, j) = (model_cost(M, train_input, train_output) - cost) / epsilon;
+            MATRIX_AT(M.b2, i, j) = saved;
+        }
+    }
+}
+
+void learninXor(neuralNet M, neuralNet g, float rate)
+{
+    for (size_t i = 0; i < M.w1.rows; i++){
+        for (size_t j = 0; j < M.w1.cols; j++){
+            MATRIX_AT(M.w1, i, j) -= rate * MATRIX_AT(g.w1, i, j);
+        }
+    }
+    for (size_t i = 0; i < M.b1.rows; i++){
+        for (size_t j = 0; j < M.b1.cols; j++){
+            MATRIX_AT(M.b1, i, j) -= rate * MATRIX_AT(g.b1, i, j);
+        }
+    }
+    for (size_t i = 0; i < M.w2.rows; i++){
+        for (size_t j = 0; j < M.w2.cols; j++){
+            MATRIX_AT(M.w2, i, j) -= rate * MATRIX_AT(g.w2, i, j);
+        }
+    }
+    for (size_t i = 0; i < M.b2.rows; i++){
+        for (size_t j = 0; j < M.b2.cols; j++){
+            MATRIX_AT(M.b2, i, j) -= rate * MATRIX_AT(g.b2, i, j);
+        }
+    }
+}
+
 int main(){
     srand(time(NULL));
-    Matrix w1 = matrix_init(2, 2);
-    Matrix b1 = matrix_init(1, 2);
-    Matrix w2 = matrix_init(2, 1);
-    Matrix b2 = matrix_init(1, 1);
 
-    matrix_randomize(w1, 0, 1);
-    matrix_randomize(b1, 0, 1);
-    matrix_randomize(w2, 0, 1);
-    matrix_randomize(b2, 0, 1);
-
-    matrix_print(w1);
-    matrix_print(b1);
-    matrix_print(w2);
-    matrix_print(b2);
+    size_t n = sizeof(td) / sizeof(td[0]) / 3;
+    Matrix train_input = matrix_init(n, 2);
+    Matrix train_output = matrix_init(n, 1);
+    for (size_t i = 0; i < n; i++){
+        MATRIX_AT(train_input, i, 0) = td[i * 3 + 0];
+        MATRIX_AT(train_input, i, 1) = td[i * 3 + 1];
+        MATRIX_AT(train_output, i, 0) = td[i * 3 + 2];
+    }
+    neuralNet g = XorInit(2, 2, 1);
+    neuralNet M = XorInit(2, 2, 1);
+    matrix_randomize(M.w1, 0, 1);
+    matrix_randomize(M.b1, 0, 1);
+    matrix_randomize(M.w2, 0, 1);
+    matrix_randomize(M.b2, 0, 1);
+    
+    printf("cost: %f\n", model_cost(M, train_input, train_output));
+    while (model_cost(M, train_input, train_output) > 0.0001){
+        finite_difference(M, g, 0.1, train_input, train_output);
+        learninXor(M, g, 0.1);
+        printf("cost: %f\n", model_cost(M, train_input, train_output));
+    }
+    printf("cost: %f\n", model_cost(M, train_input, train_output));
+    for (size_t i = 0; i < 2; i++){
+        for (size_t j = 0; j < 2; j++){
+            MATRIX_AT(M.a0, 0, 0) = i;
+            MATRIX_AT(M.a0, 0, 1) = j;
+            XorForward(M);
+            float y_hat = MATRIX_AT(M.a2, 0, 0);
+            printf("x1: %f, x2: %f, y_hat: %f\n", (float)i, (float)j, y_hat);
+        }
+    }
     return 0;
 }
--- brain.h
+++ brain.h
@@ -10,6 +10,7 @@
 #include <unistd.h>
 
 typedef struct {
+    size_t stride;
     size_t rows;
     size_t cols;
     float *data;
@@ -18,9 +19,29 @@ typedef struct {
 Matrix matrix_init(size_t rows, size_t cols);
 void matrix_randomize(Matrix M, float min, float max);
 void matrix_fill(Matrix M, float val);
+void matrix_copy(Matrix M, Matrix A);
+void matrix_row(Matrix M, size_t row, Matrix A);
+void matrix_col(Matrix M, size_t col, Matrix A);
 void matrix_dot(Matrix M, Matrix A, Matrix B);
 void matrix_add(Matrix M, Matrix A, Matrix B);
-void matrix_print(Matrix M);
+void matrix_print(Matrix M, const char *name);
+void sigmoid_activation(Matrix M);
+#define MATRIX_AT(M, i, j) M.data[i * M.cols + j]
+#define MATRIX_PRINT(M) matrix_print(M, #M)
+#define sigmoid(x) (1.0f / (1.0f + exp(-x)))
+#define sigmoid_derivative(x) (x * (1.0f - x))
+#define relu(x) (x > 0.0f ? x : 0.0f)
+#define relu_derivative(x) (x > 0.0f ? 1.0f : 0.0f)
+#define tanh(x) (2 / (1.0f + exp(-2 * x)) - 1.0f)
+#define tanh_derivative(x) (1.0f - x * x)
+#define leaky_relu(x) (x > 0.0f ? x : 0.01f * x)
+#define leaky_relu_derivative(x) (x > 0.0f ? 1.0f : 0.01f)
+#define identity(x) (x)
+#define identity_derivative(x) (1f)
+#define softmax(x) (exp(x) / exp(x).sum())
+#define softmax_derivative(x) (x * (1f - x))
+#define cross_entropy(x, y) (-log(x[y]))
+#define cross_entropy_derivative(x, y) (x - y)
 
 #endif
 
@@ -51,6 +72,34 @@ void matrix_dot(Matrix M, Matrix A, Matrix B) {
     }
 }
 
+void matrix_copy(Matrix M, Matrix A) {
+    if (M.rows != A.rows || M.cols != A.cols) {
+        printf("Error: matrix dimensions do not match\n");
+        exit(1);
+    }
+    memcpy(M.data, A.data, M.rows * M.cols * sizeof(*M.data));
+}
+
+void matrix_row(Matrix M, size_t row, Matrix A) {
+    if (M.cols != A.cols) {
+        printf("Error: matrix dimensions do not match\n");
+        exit(1);
+    }
+    for (size_t i = 0; i < M.cols; i++) {
+        M.data[i] = A.data[row * A.cols + i];
+    }
+}
+
+void matrix_col(Matrix M, size_t col, Matrix A) {
+    if (M.rows != A.rows) {
+        printf("Error: matrix dimensions do not match\n");
+        exit(1);
+    }
+    for (size_t i = 0; i < M.rows; i++) {
+        M.data[i * M.cols + col] = A.data[i * A.cols];
+    }
+}
+
 void matrix_add(Matrix M, Matrix A, Matrix B) {
     if (A.rows != B.rows || A.cols != B.cols) {
         printf("Error: matrix dimensions do not match\n");
@@ -63,7 +112,8 @@ void matrix_add(Matrix M, Matrix A, Matrix B) {
     }
 }
 
-void matrix_print(Matrix M) {
+void matrix_print(Matrix M, const char *name) {
+    printf("%s:\n", name);
     for (size_t i = 0; i < M.rows; i++) {
         printf("| ");
         for (size_t j = 0; j < M.cols; j++) {
@@ -86,6 +136,14 @@ void matrix_randomize(Matrix M, float min, float max) {
     for (size_t i = 0; i < M.rows; i++) {
         for (size_t j = 0; j < M.cols; j++) {
             M.data[i * M.cols + j] = ((float)rand() / RAND_MAX) * (max - min) + min;
+        }
+    }
+}
+
+void sigmoid_activation(Matrix M) {
+    for (size_t i = 0; i < M.rows; i++) {
+        for (size_t j = 0; j < M.cols; j++) {
+            M.data[i * M.cols + j] = sigmoid(M.data[i * M.cols + j]);
         }
     }
 }
